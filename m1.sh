4a8c7c7425400c5913c16b0c9158793adfd84201
jayram Singh <jayrambpl@gmail.com>
2021-05-26 11:55:52 +0530

    f1

--- m1.sh
+++ m1.sh
@@ -59,4 +59,13 @@ my_function() {
 my_function
 GLOBAL_VAR=1
 
-### 
+### f1 #### 
+#!/bin/bash
+
+logger "Message"
+logger -p local0.info "Message"
+logger -s -p local0.info "Message"
+logger -t myscript -p local0.info "Message"
+logger -i -t myscript "Message"
+
+###
