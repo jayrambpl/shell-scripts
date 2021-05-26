6058721c0e933c03eaa79e1307e3e7abdbbf7d62
jayram Singh <jayrambpl@gmail.com>
2021-05-26 11:52:45 +0530

    m3

--- m1.sh
+++ m1.sh
@@ -45,3 +45,18 @@ else
   # Abort the script and return a non-zero exit status.
   exit 1
 fi
+
+
+###  3 ###
+
+#!/bin/bash
+
+my_function() {
+    echo "$GLOBAL_VAR"
+}
+
+# The value of GLOBAL_VAR is NOT available to my_function since GLOBAL_VAR was defined after my_function was called.
+my_function
+GLOBAL_VAR=1
+
+### 
